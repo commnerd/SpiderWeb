dcb3edcf85a2eea48fd0d45f1ab6a4d1bbddd6f1
Michael J. Miller <commnerd@gmail.com>
2021-02-13 21:09:10 -0700

    Added provisioning script for ubuntu control plane

---
+++ control_plane/provisioning/ubuntu/provision.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo su -
+
+apt-get update && apt-get install -y \
+    software-properties-common \
+    apt-transport-https \
+    ca-certificates \
+    supervisor \
+    docker.io \
+    curl
+
+# Set up the Docker daemon
+cat <<EOF | tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+service docker restart
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
+    echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list && \
+    apt-get update && \
+    apt-get install -y kubelet kubeadm kubectl && \
+    apt-mark hold kubelet kubeadm kubectl
+
+kubeadm init --control-plane-endpoint spiderweb.com
+
+# Allow controle plane to run pods
+kubectl taint nodes --all node-role.kubernetes.io/master-
